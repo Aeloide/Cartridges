513e1a552e108b624ae341e6334a0fed6d4b412f
Aeloide <72701764+Aeloide@users.noreply.github.com>
2022-05-13 21:10:16 +0300

    1

--- global.php
+++ global.php
@@ -1,19 +1,15 @@
 <?php
 	date_default_timezone_set('Europe/Moscow');
 	session_start();
-	require("mysqli.class.php");
+	$projectName = 'Cartridges-1.1';
+	require('assets/'.$projectName."/mysqli.class.php");
 	$DB = new sql_mysql;	
 	require("connect_settings.php");
 //	$DB->connect('127.0.0.1','user','password','database',0);
 	$DB->query("USE `cartriges`");
 	$DB->query("SET NAMES 'utf8mb4'");
 
-	function show_select($selected, $array, $selectname){
-		//формирует select в html из массива
-		$return = '<select name="'.$selectname.'" id="'.$selectname.'">';
-		foreach($array as $key => $value) $return .= '<option value="'.$key.'"'.(($selected == $key) ? ' selected="selected"' : '').'>'.strip_tags($value).'</option>';
-		return $return."</select>\n";
-	}
-
+	require('assets/'.$projectName."/functions.php");
+	
 
 ?>
